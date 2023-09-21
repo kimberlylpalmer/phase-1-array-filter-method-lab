17fd9560f8f9b92350095bd27cce3b95f41f4e12
Kimberly L Palmer <kimberlylpalmer@gmail.com>
2023-09-20 23:23:51 -0700

    working my way through another lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+function findMatching(collection, string) {
+    return collection.filter(name => name.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(collection, string) {
+    return collection.filter(name => name[0] === string[0])
+   
+}
+
+function matchName(collection, string) {
+    return collection.filter(obj => obj.name === string)
+}
+    
+
