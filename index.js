bdead8f71f813ceea61b05fe620fde3fdbf55c4b
Kimberly Palmer <kimberlylpalmer@gmail.com>
2023-11-05 19:36:10 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 // Code your solution here
 
-function findMatching(collection, string) {
-    return collection.filter(name => name.toLowerCase() === string.toLowerCase())
+function findMatching(collection, str) {
+    return collection.filter(name => name.toLowerCase() === str.toLowerCase())
 }
 
-function fuzzyMatch(collection, string) {
-    return collection.filter(name => name[0] === string[0])
+function fuzzyMatch(collection, str) {
+    return collection.filter(name => name[0] === str[0])
+}
+
+function matchName(collection, str) {
+    return collection.filter(obj => obj.name === str)
+}
+
+
+
+// function findMatching(collection, string) {
+//     return collection.filter(name => name.toLowerCase() === string.toLowerCase())
+// }
+
+// function fuzzyMatch(collection, string) {
+//     return collection.filter(name => name[0] === string[0])
    
-}
+// }
 
-function matchName(collection, string) {
-    return collection.filter(obj => obj.name === string)
-}
+// function matchName(collection, string) {
+//     return collection.filter(obj => obj.name === string)
+// }
     
 
